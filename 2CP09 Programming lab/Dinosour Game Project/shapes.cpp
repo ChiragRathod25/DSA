6f097f1f651c80e75dceccf250cc92f1b5d128da
Chirag Rathod <chiragrathod9987@gmail.com>
2024-03-28 15:30:32 +0530

    opengl_practise

---
+++ 2CP09 Programming lab/Dinosour Game Project/shapes.cpp
@@ -0,0 +1,62 @@
+#include <windows.h>
+#include "GL/glut.h"
+using namespace std;
+void Display()
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    // line
+    glBegin(GL_LINE);
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glVertex2f(1, 1);
+    glVertex2f(2, 2);
+    glEnd();
+    
+    // rectangle
+    glBegin(GL_QUADS);           // Each set of 4 vertices form a quad
+    glColor3f(1.0f, 0.0f, 0.0f); // Red
+    glVertex2f(-0.8f, 0.1f);     // Define vertices in counter-clockwise (CCW) order
+    glVertex2f(-0.2f, 0.1f);     //  so that the normal (front-face) is facing you
+    glVertex2f(-0.2f, 1);
+    glVertex2f(-0.8f, 1);
+    glEnd();
+
+    // triangle
+    glBegin(GL_TRIANGLES); // Each set of 4 vertices form a quad
+    glColor3f(0.1f, 0.3f, 1.0f);
+    glVertex2f(0, 0);
+    glVertex2f(0.8f, 0);
+    glVertex2f(0.4f, 0.8f);
+    glEnd();
+
+    //polygon
+    glBegin(GL_POLYGON);         // These vertices form a closed polygon
+    glColor3f(1.0f, 1.0f, 0.0f); // Yellow
+    glVertex2f(0.4f, 0.2f);
+    glVertex2f(0.6f, 0.2f);
+    glVertex2f(0.7f, 0.4f);
+    glVertex2f(0.6f, 0.6f);
+    glVertex2f(0.4f, 0.6f);
+    glVertex2f(0.3f, 0.4f);
+    glEnd();
+
+    glFlush();
+}
+void Init()
+{   
+    glClearColor(0, 0, 0, 0);
+}
+void reshape(int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+// void reshape(){}
+int main(int argc, char *argv[])
+{
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowSize(321, 240);
+    glutCreateWindow("Chirag Rathod");
+    Init();
+    glutDisplayFunc(Display);
+    glutReshapeFunc(reshape);
+    glutMainLoop();
+}
