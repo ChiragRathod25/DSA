7cb0bd2092474d40fb78ebb432ff35905038a4c3
Chirag Rathod <chiragrathod9987@gmail.com>
2024-04-26 11:41:32 +0530

    sortUpdate

--- 2CP06 DSA/Sorting/redixSort.cpp
+++ 2CP06 DSA/Sorting/redixSort.cpp
@@ -1,19 +1,64 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-void display(int *arr,int arrSize){
-    for(int i=0;i<arrSize;i++){
-        cout<<arr[i]<<"\t";
-    }
-    cout<<endl;
-}
-int main(){
-    int arr[]={10,5,25,7,50,1};
-    int arrSize=sizeof(arr)/sizeof(arr[0]);
-    cout<<"Selection sort : "<<endl;
-    display(arr,arrSize);
-    
 
-    display(arr,arrSize);
-    
+int getMax(vector<int> &arr) {
+    int max = arr[0];
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i] > max) {
+            max = arr[i];   
+        }
+    }
+    return max;
+}
+
+void countSort(vector<int> &arr, int n, int exp) {
+    vector<int> output(n), count(10, 0);
+
+    for(int i = 0; i < n; i++) {
+        count[(arr[i]/exp)%10]++;
+    }
+
+    for(int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for(int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i]/exp)%10] - 1] = arr[i];
+        count[(arr[i]/exp)%10]--;
+    }
+
+    for(int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
+
+void radixSort(vector<int> &arr) {
+    int n = arr.size();
+    int m = getMax(arr);
+
+    for(int exp = 1; m/exp > 0; exp *= 10) {
+        countSort(arr, n, exp);
+    }
+}
+
+void printArray(vector<int> &arr) {
+    for (int i = 0; i < arr.size(); i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> arr = {170, 45, 75, 90, 802, 24, 2, 66};
+
+    cout << "Original array: ";
+    printArray(arr);
+
+    radixSort(arr);
+
+    cout << "Sorted array: ";
+    printArray(arr);
+
     return 0;
 }
